e69e11266d5bdd957f1fc45badb4a491a4b82225
Frz-Ehr <136071031+Frz-Ehr@users.noreply.github.com>
2023-06-10 18:38:15 +0200

    Update gpu.py

--- gpu.py
+++ gpu.py
@@ -1,10 +1,13 @@
 import bpy
 
-
-def enable_gpus(device_type, use_cpus=False):
+def get_all_devices():
     preferences = bpy.context.preferences
     cycles_preferences = preferences.addons["cycles"].preferences
     optix_devices, cuda_devices = cycles_preferences.get_devices()
+    return optix_devices, cuda_devices
+
+def enable_gpus(device_type, use_cpus=False):
+    optix_devices, cuda_devices = get_all_devices()
 
     if device_type == "OPTIX":
         devices = optix_devices
@@ -27,5 +30,20 @@ def enable_gpus(device_type, use_cpus=False):
 
     return activated_gpus
 
+# Print all available devices
+optix_devices, cuda_devices = get_all_devices()
+print("Available CUDA devices: ", [device.name for device in cuda_devices])
+print("Available OPTIX devices: ", [device.name for device in optix_devices])
 
-enable_gpus("OPTIX")
+# Request user input for device type
+device_type = input("Enter device type (CUDA/OPTIX): ")
+
+# Request user input for CPU usage
+use_cpu = input("Do you want to use CPUs for rendering as well? (yes/no): ")
+use_cpu = use_cpu.lower() == "yes"
+
+# Enable GPUs
+activated_gpus = enable_gpus(device_type, use_cpu)
+
+# Print activated GPUs
+print("Activated GPUs: ", activated_gpus)
