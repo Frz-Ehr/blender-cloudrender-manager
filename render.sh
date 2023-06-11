d96550a84b2d707717afb4cbe89326c5e17ac773
Frz-Ehr <136071031+Frz-Ehr@users.noreply.github.com>
2023-06-11 11:11:14 +0200

    Update render.sh

--- render.sh
+++ render.sh
@@ -30,21 +30,35 @@ while true; do
     # Ask user for the render type
     read -p "Enter 1 for image render, 2 for video render: " render_type
 
+    # Ask user for the device type
+    echo "Enter 1 to use CUDA, 2 to use OptiX"
+    read -p "Please enter the number of the device type you want to use for rendering: " device_type
+
+    if [[ $device_type -eq 1 ]]; then
+        device_flag="CUDA"
+    elif [[ $device_type -eq 2 ]]; then
+        device_flag="OPTIX"
+    else
+        echo "Invalid device type entered. Please enter 1 to use CUDA, 2 to use OptiX."
+        continue
+    fi
+
     if [[ $render_type -eq 1 ]]; then
         # Ask user for the frame to render
         read -p "Please enter the frame number you want to render: " frame_number
 
         # Start rendering
-        echo "Rendering frame $frame_number..."
-        ./blender -b $blend_file -P gpu.py -o output/ -f $frame_number
+        echo "Rendering frame $frame_number using $device_flag..."
+        ./blender -b $blend_file -E CYCLES -o output/ -f $frame_number -- --cycles-device $device_flag
     elif [[ $render_type -eq 2 ]]; then
-        echo "Rendering animation..."
-        ./blender -b $blend_file -P gpu.py -o output/ -a
+        echo "Rendering animation using $device_flag..."
+        ./blender -b $blend_file -E CYCLES -o output/ -a -- --cycles-device $device_flag
     else
         echo "Invalid render type entered. Please enter 1 for image render, 2 for video render."
         continue
     fi
 
     echo "Rendering complete. The output can be found in the output/ directory."
+    cd ..
     ./main.sh
 done
